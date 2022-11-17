68cbcb171a3cd09187b03a0b6be6deac080148c7
0arari0 <110008465+0arari0@users.noreply.github.com>
2022-11-17 15:29:23 +0900

    221117 start
    발판 & 플립

---
+++ Assets/00_Scripts/Bounce.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.y<=0f)
+        {
+            Rigidbody2D rigid = collision.gameObject.GetComponent<Rigidbody2D>();
+            if(rigid!=null)
+            {
+                Vector2 velocity = rigid.velocity;
+                velocity.y = jumpForce;
+                rigid.velocity = velocity;
+            }
+        }
+        
+    }
+}
---
+++ Assets/00_Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Rigidbody2D rigid;
+    public Animator anim;
+
+    private float moveX;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        Flip();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rigid.velocity;
+        velocity.x = moveX;
+        rigid.velocity = velocity;
+    }
+    void Flip()
+    {
+        Vector3 flipMove = Vector3.zero;
+
+        if(Input.GetAxisRaw("Horizontal")<0)
+        {
+            flipMove = Vector3.left;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if(Input.GetAxisRaw("Horizontal")>0)
+        {
+            flipMove = Vector3.right;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += flipMove * 5f * Time.deltaTime;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            anim.SetBool("Collision", true);
+        }
+        else
+        {
+            anim.SetBool("Collision", false);
+        }
+
+    }
+
+}
