72c7b8b38e9e9835555e6405dd7969cf6ad96ec2
0arari0 <110008465+0arari0@users.noreply.github.com>
2022-12-01 17:30:43 +0900

    22.12.01 update
    - 부서지는 발판 이미지 변환 구현

--- Assets/00_Scripts/BreakPlatform.cs
+++ Assets/00_Scripts/BreakPlatform.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BreakPlatform : MonoBehaviour
 {
     public float fallSec = 0.5f;
     public float destroySec = 2f;
     Rigidbody2D rigid;
+    public Sprite breakImg; //바꿀 이미지
+    SpriteRenderer NowImg; //현재 이미지
 
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        NowImg= GetComponent<SpriteRenderer>();
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag=="Player")
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {   // 충돌한 객체가 플레이어이고, 부딪힌 순간 플레이어의 상대속도가 0보다 작다면(위에서 아래로 내려오고 있는 중이라면)
+        if (collision.gameObject.tag == "Player" && collision.relativeVelocity.y < 0f)
         {
+            NowImg.sprite = breakImg;
             Invoke("FallPlatform", fallSec); //fallPlatform함수 실행 fallsec만큼
             Destroy(gameObject, destroySec); //게임오브젝트 파괴
         }
@@ -24,4 +30,5 @@ public class BreakPlatform : MonoBehaviour
     {
         rigid.isKinematic = false;
     }
+   
 }
